e504ee0fe8c284c283f79c7a9d9ebf8f0a526b44
Bhagyesh patil <bhagyeshpatil018@gmail.com>
2023-12-04 20:00:59 +0530

    new implication

--- cypress/e2e/practice/amazon_dropdown.cy.js
+++ cypress/e2e/practice/amazon_dropdown.cy.js
@@ -4,6 +4,11 @@ require('cypress-xpath')
 const commonUseCase = require('../../commonFile/amazon')
 const element = require('../../POM/amazonElement.json')
 
+
+const data ={
+    text: "Explore More With PC Jeweller"
+}
+
 describe("amazon", () => {
     before("open amazon webside", () => {
         commonUseCase.openAmzonUrl()
@@ -15,6 +20,6 @@ describe("amazon", () => {
             .type('{enter}')
         commonUseCase.verifyText().should('have.text', 'Pure Gold Pendants')
         cy.get(element.searchBarId).clear().type("silver").type('{enter}')
-        commonUseCase.verifyText().should('have.text', 'Colloidal Silver Supplement By Asterveda')
+        commonUseCase.verifyText().should('have.text', data.text)
     })
 })
